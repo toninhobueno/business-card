080736c7d1e9b91814f8749b02ac547c2c1363f5
tonin <antonio.leite00@hotmail.com>
2021-09-26 01:27:01 -0300

    Configuração inicial do Room Database

--- app/src/main/java/github/toninhobueno/businesscard/App.kt
+++ app/src/main/java/github/toninhobueno/businesscard/App.kt
@@ -1,6 +1,11 @@
 package github.toninhobueno.businesscard
 
 import android.app.Application
+import github.toninhobueno.businesscard.data.AppDataBase
+import github.toninhobueno.businesscard.data.BusinessCardRepository
 
 class App : Application() {
+    val database by lazy {AppDataBase.getDatabase(this) }
+    val repository by lazy {BusinessCardRepository(database.businessDao())}
+
 }
---
+++ app/src/main/java/github/toninhobueno/businesscard/data/BusinessCard.kt
@@ -0,0 +1,15 @@
+package github.toninhobueno.businesscard.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class BusinessCard(
+    @PrimaryKey(autoGenerate = true) val id : Int = 0,
+    val nome: String,
+    val empresa : String,
+    val telefone : String,
+    val email : String,
+    val fundoPersonalizado : String
+)
